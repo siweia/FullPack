8195424002d926a16075bf5efc0b4ba9e99ada9d
siweia <siwei1990@outlook.com>
2017-09-01 01:10:22 +0800

    世界任务列表字体阴影调整

--- Interface/AddOns/AngryWorldQuests/QuestFrame.lua
+++ Interface/AddOns/AngryWorldQuests/QuestFrame.lua
@@ -480,7 +480,7 @@ local function UpdateTitleButton(title)
 		title.TagTexture:SetSize(16, 16)
 		title.TagText:ClearAllPoints()
 		title.TagText:SetPoint("RIGHT", title.TagTexture , "LEFT", -3, 0)
-		title.TagText:SetFont(filename, fontHeight, "")
+		title.TagText:SetFont(filename, fontHeight, "OUTLINE")
 	end
 end
 local function UpdateTitleButtons()
