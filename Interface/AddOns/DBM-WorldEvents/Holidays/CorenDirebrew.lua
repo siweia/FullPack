c7faf8bdfd09d5b0d79edc3c55cc0ece02074630
siweia <siwei1990@gmail.com>
2020-01-16 08:13:59 +0800

    DBM更新到8.3.0版本

--- Interface/AddOns/DBM-WorldEvents/Holidays/CorenDirebrew.lua
+++ Interface/AddOns/DBM-WorldEvents/Holidays/CorenDirebrew.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("d287", "DBM-WorldEvents", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20191011015318")
+mod:SetRevision("20191024200504")
 mod:SetCreatureID(23872)
 mod:SetModelID(21824)
 mod:SetReCombatTime(10)
