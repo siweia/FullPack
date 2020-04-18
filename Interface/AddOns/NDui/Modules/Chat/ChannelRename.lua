dc2421d90b4e30fdc624be264e81e247fc6ca81c
siweia <siwei1990@gmail.com>
2020-04-19 03:17:43 +0800

    聊天时间戳调整

--- Interface/AddOns/NDui/Modules/Chat/ChannelRename.lua
+++ Interface/AddOns/NDui/Modules/Chat/ChannelRename.lua
@@ -2,15 +2,34 @@
 local B, C, L, DB = unpack(ns)
 local module = B:GetModule("Chat")
 
-local gsub, strfind = string.gsub, string.find
+local gsub, strfind, strmatch = string.gsub, string.find, string.match
+local BetterDate, time = BetterDate, time
 local INTERFACE_ACTION_BLOCKED = INTERFACE_ACTION_BLOCKED
 
+local timestampFormat = {
+	[2] = "%I:%M %p ",
+	[3] = "%I:%M:%S %p ",
+	[4] = "%H:%M ",
+	[5] = "%H:%M:%S ",
+}
 function module:UpdateChannelNames(text, ...)
 	if strfind(text, INTERFACE_ACTION_BLOCKED) and not DB.isDeveloper then return end
 
 	local r, g, b = ...
 	if NDuiDB["Chat"]["WhisperColor"] and strfind(text, L["Tell"].." |H[BN]*player.+%]") then
 		r, g, b = r*.7, g*.7, b*.7
+	end
+
+	-- Dev logo
+	local unitName = strmatch(text, "|Hplayer:([^|:]+)")
+	if unitName and DB.Devs[unitName] then
+		text = gsub(text, "(|Hplayer.+)", "|T"..DB.chatIcon..":14:24|t%1")
+	end
+
+	-- Timestamp
+	if NDuiADB["TimestampFormat"] > 1 then
+		local timeStamp = BetterDate(DB.GreyColor..timestampFormat[NDuiADB["TimestampFormat"]].."|r", time())
+		text = timeStamp..text
 	end
 
 	if NDuiDB["Chat"]["Oldname"] then
