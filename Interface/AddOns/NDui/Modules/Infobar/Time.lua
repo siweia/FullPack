e80f13a38eec5c6bf879fab24e34cf80d134ca9f
siweia <siwei1990@outlook.com>
2020-12-16 02:39:21 +0800

    信息条时间模块更新，添加托斯加特的信息

--- Interface/AddOns/NDui/Modules/Infobar/Time.lua
+++ Interface/AddOns/NDui/Modules/Infobar/Time.lua
@@ -5,7 +5,7 @@ if not C.Infobar.Time then return end
 local module = B:GetModule("Infobar")
 local info = module:RegisterInfobar("Time", C.Infobar.TimePos)
 local time, date = time, date
-local strfind, format, floor = string.find, string.format, math.floor
+local strfind, format, floor, strmatch = strfind, format, floor, strmatch
 local mod, tonumber, pairs, ipairs, select = mod, tonumber, pairs, ipairs, select
 local C_Map_GetMapInfo = C_Map.GetMapInfo
 local C_DateAndTime_GetCurrentCalendarTime = C_DateAndTime.GetCurrentCalendarTime
@@ -13,13 +13,15 @@ local C_Calendar_SetAbsMonth = C_Calendar.SetAbsMonth
 local C_Calendar_OpenCalendar = C_Calendar.OpenCalendar
 local C_Calendar_GetNumDayEvents = C_Calendar.GetNumDayEvents
 local C_Calendar_GetNumPendingInvites = C_Calendar.GetNumPendingInvites
+local C_AreaPoiInfo_GetAreaPOIInfo = C_AreaPoiInfo.GetAreaPOIInfo
 local C_AreaPoiInfo_GetAreaPOISecondsLeft = C_AreaPoiInfo.GetAreaPOISecondsLeft
 local C_IslandsQueue_GetIslandsWeeklyQuestID = C_IslandsQueue.GetIslandsWeeklyQuestID
+local C_UIWidgetManager_GetTextWithStateWidgetVisualizationInfo = C_UIWidgetManager.GetTextWithStateWidgetVisualizationInfo
 local TIMEMANAGER_TICKER_24HOUR, TIMEMANAGER_TICKER_12HOUR = TIMEMANAGER_TICKER_24HOUR, TIMEMANAGER_TICKER_12HOUR
 local FULLDATE, CALENDAR_WEEKDAY_NAMES, CALENDAR_FULLDATE_MONTH_NAMES = FULLDATE, CALENDAR_WEEKDAY_NAMES, CALENDAR_FULLDATE_MONTH_NAMES
 local PLAYER_DIFFICULTY_TIMEWALKER, RAID_INFO_WORLD_BOSS, DUNGEON_DIFFICULTY3 = PLAYER_DIFFICULTY_TIMEWALKER, RAID_INFO_WORLD_BOSS, DUNGEON_DIFFICULTY3
 local DUNGEONS, RAID_INFO, QUESTS_LABEL, ISLANDS_HEADER, QUEST_COMPLETE = DUNGEONS, RAID_INFO, QUESTS_LABEL, ISLANDS_HEADER, QUEST_COMPLETE
-local PVP_CONQUEST, LFG_LIST_LOADING, QUEUE_TIME_UNAVAILABLE = PVP_CONQUEST, LFG_LIST_LOADING, QUEUE_TIME_UNAVAILABLE
+local PVP_CONQUEST, LFG_LIST_LOADING, QUEUE_TIME_UNAVAILABLE, RATED_PVP_WEEKLY_VAULT, UNKNOWN = PVP_CONQUEST, LFG_LIST_LOADING, QUEUE_TIME_UNAVAILABLE, RATED_PVP_WEEKLY_VAULT, UNKNOWN
 local HORRIFIC_VISION = SPLASH_BATTLEFORAZEROTH_8_3_0_FEATURE1_TITLE
 local RequestRaidInfo, UnitLevel, GetNumSavedWorldBosses, GetSavedWorldBossInfo = RequestRaidInfo, UnitLevel, GetNumSavedWorldBosses, GetSavedWorldBossInfo
 local GetCVarBool, GetGameTime, GameTime_GetLocalTime, GameTime_GetGameTime, SecondsToTime = GetCVarBool, GetGameTime, GameTime_GetLocalTime, GameTime_GetGameTime, SecondsToTime
@@ -190,6 +192,20 @@ local function GetNzothThreatName(questID)
 	return name
 end
 
+-- Torghast
+local TorghastWidgets = {
+	{nameID = 2925, levelID = 2930}, -- Fracture Chambers
+	{nameID = 2926, levelID = 2932}, -- Skoldus Hall
+	{nameID = 2924, levelID = 2934}, -- Soulforges
+	{nameID = 2927, levelID = 2936}, -- Coldheart Interstitia
+	{nameID = 2928, levelID = 2938}, -- Mort'regar
+	{nameID = 2929, levelID = 2940}, -- The Upper Reaches
+}
+
+local function CleanupLevelName(text)
+	return gsub(text, "|n", "")
+end
+
 local title
 local function addTitle(text)
 	if not title then
@@ -242,6 +258,22 @@ info.onEnter = function(self)
 			addTitle(RAID_INFO)
 			if extended then r,g,b = .3,1,.3 else r,g,b = 1,1,1 end
 			GameTooltip:AddDoubleLine(name.." - "..diffName, SecondsToTime(reset, true, nil, 3), 1,1,1, r,g,b)
+		end
+	end
+
+	-- Torghast
+	local TorghastInfo = C_AreaPoiInfo_GetAreaPOIInfo(1543, 6640)
+	if IsQuestFlaggedCompleted(60136) and TorghastInfo then
+		title = false
+		for _, value in pairs(TorghastWidgets) do
+			local nameInfo = C_UIWidgetManager_GetTextWithStateWidgetVisualizationInfo(value.nameID)
+			if nameInfo and nameInfo.shownState == 1 then
+				addTitle(TorghastInfo.name)
+				local nameText = CleanupLevelName(nameInfo.text)
+				local levelInfo = C_UIWidgetManager_GetTextWithStateWidgetVisualizationInfo(value.levelID)
+				local levelText = levelInfo and CleanupLevelName(levelInfo.text) or UNKNOWN
+				GameTooltip:AddDoubleLine(nameText, levelText)
+			end
 		end
 	end
 
@@ -339,6 +371,7 @@ info.onEnter = function(self)
 	-- Help Info
 	GameTooltip:AddDoubleLine(" ", DB.LineString)
 	GameTooltip:AddDoubleLine(" ", DB.LeftButton..L["Toggle Calendar"].." ", 1,1,1, .6,.8,1)
+	GameTooltip:AddDoubleLine(" ", DB.ScrollButton..RATED_PVP_WEEKLY_VAULT.." ", 1,1,1, .6,.8,1)
 	GameTooltip:AddDoubleLine(" ", DB.RightButton..L["Toggle Clock"].." ", 1,1,1, .6,.8,1)
 	GameTooltip:Show()
 end
@@ -348,6 +381,13 @@ info.onLeave = B.HideTooltip
 info.onMouseUp = function(_, btn)
 	if btn == "RightButton" then
 		ToggleTimeManager()
+	elseif btn == "MiddleButton" then
+		if not WeeklyRewardsFrame then LoadAddOn("Blizzard_WeeklyRewards") end
+		if InCombatLockdown() then
+			B:TogglePanel(WeeklyRewardsFrame)
+		else
+			ToggleFrame(WeeklyRewardsFrame)
+		end
 	else
 		if InCombatLockdown() then UIErrorsFrame:AddMessage(DB.InfoColor..ERR_NOT_IN_COMBAT) return end
 		ToggleCalendar()
