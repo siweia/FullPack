4f96d872ef877c5b0868b5fc93419f81d63c287b
siweia <siwei1990@outlook.com>
2021-01-10 20:33:43 +0800

    添加小队监控的预设

--- Interface/AddOns/NDui/Core/ExtraGUI.lua
+++ Interface/AddOns/NDui/Core/ExtraGUI.lua
@@ -416,12 +416,17 @@ function G:SetupClickCast(parent)
 	end
 end
 
+local function updatePartyWatcherSpells()
+	B:GetModule("UnitFrames"):UpdatePartyWatcherSpells()
+end
+
 function G:SetupPartyWatcher(parent)
 	local guiName = "NDuiGUI_PartyWatcher"
 	toggleExtraGUI(guiName)
 	if extraGUIs[guiName] then return end
 
 	local panel = createExtraGUI(parent, guiName, L["AddPartyWatcher"].."*", true)
+	panel:SetScript("OnHide", updatePartyWatcherSpells)
 
 	local barTable = {}
 	local ARCANE_TORRENT = GetSpellInfo(25046)
@@ -440,7 +445,11 @@ function G:SetupPartyWatcher(parent)
 		B.AddTooltip(icon, "ANCHOR_RIGHT", spellID, "system")
 		close:SetScript("OnClick", function()
 			bar:Hide()
-			NDuiADB["PartyWatcherSpells"][spellID] = nil
+			if C.PartySpells[spellID] then
+				NDuiADB["PartySpells"][spellID] = 0
+			else
+				NDuiADB["PartySpells"][spellID] = nil
+			end
 			barTable[spellID] = nil
 			sortBars(barTable)
 		end)
@@ -464,14 +473,15 @@ function G:SetupPartyWatcher(parent)
 	options[2] = G:CreateEditbox(frame, L["Cooldown*"], 120, -30, L["Cooldown Intro"])
 
 	local scroll = G:CreateScroll(frame, 240, 410)
-	scroll.reset = B.CreateButton(frame, 70, 25, RESET)
+	scroll.reset = B.CreateButton(frame, 55, 25, RESET)
 	scroll.reset:SetPoint("TOPLEFT", 10, -80)
+	scroll.reset.text:SetTextColor(1, 0, 0)
 	StaticPopupDialogs["RESET_NDUI_PARTYWATCHER"] = {
 		text = L["Reset your raiddebuffs list?"],
 		button1 = YES,
 		button2 = NO,
 		OnAccept = function()
-			wipe(NDuiADB["PartyWatcherSpells"])
+			wipe(NDuiADB["PartySpells"])
 			ReloadUI()
 		end,
 		whileDead = 1,
@@ -484,27 +494,77 @@ function G:SetupPartyWatcher(parent)
 		local spellID, duration = tonumber(options[1]:GetText()), tonumber(options[2]:GetText())
 		if not spellID or not duration then UIErrorsFrame:AddMessage(DB.InfoColor..L["Incomplete Input"]) return end
 		if not GetSpellInfo(spellID) then UIErrorsFrame:AddMessage(DB.InfoColor..L["Incorrect SpellID"]) return end
-		if NDuiADB["PartyWatcherSpells"][spellID] then UIErrorsFrame:AddMessage(DB.InfoColor..L["Existing ID"]) return end
+		if NDuiADB["PartySpells"][spellID] and NDuiADB["PartySpells"][spellID] ~= 0 then UIErrorsFrame:AddMessage(DB.InfoColor..L["Existing ID"]) return end
 
-		NDuiADB["PartyWatcherSpells"][spellID] = duration
+		NDuiADB["PartySpells"][spellID] = duration
 		createBar(scroll.child, spellID, duration)
 		clearEdit(options)
 	end
 
-	scroll.add = B.CreateButton(frame, 70, 25, ADD)
+	scroll.add = B.CreateButton(frame, 55, 25, ADD)
 	scroll.add:SetPoint("TOPRIGHT", -10, -80)
 	scroll.add:SetScript("OnClick", function()
 		addClick(scroll, options)
 	end)
 
-	scroll.clear = B.CreateButton(frame, 70, 25, KEY_NUMLOCK_MAC)
-	scroll.clear:SetPoint("RIGHT", scroll.add, "LEFT", -10, 0)
+	scroll.clear = B.CreateButton(frame, 55, 25, KEY_NUMLOCK_MAC)
+	scroll.clear:SetPoint("RIGHT", scroll.add, "LEFT", -5, 0)
 	scroll.clear:SetScript("OnClick", function()
 		clearEdit(options)
 	end)
 
-	for spellID, duration in pairs(NDuiADB["PartyWatcherSpells"]) do
-		createBar(scroll.child, spellID, duration)
+	local menuList = {}
+	local function AddSpellFromPreset(_, spellID, duration)
+		options[1]:SetText(spellID)
+		options[2]:SetText(duration)
+		DropDownList1:Hide()
+	end
+
+	local index = 1
+	for class, value in pairs(C.PartySpellsDB) do
+		local color = B.HexRGB(B.ClassColor(class))
+		local localClassName = LOCALIZED_CLASS_NAMES_MALE[class]
+		menuList[index] = {text = color..localClassName, notCheckable = true, hasArrow = true, menuList = {}}
+
+		for spellID, duration in pairs(value) do
+			local spellName, _, texture = GetSpellInfo(spellID)
+			if spellName then
+				tinsert(menuList[index].menuList, {
+					text = spellName,
+					icon = texture,
+					tCoordLeft = .08,
+					tCoordRight = .92,
+					tCoordTop = .08,
+					tCoordBottom = .92,
+					arg1 = spellID,
+					arg2 = duration,
+					func = AddSpellFromPreset,
+					notCheckable = true,
+				})
+			end
+		end
+		index = index + 1
+	end
+	scroll.preset = B.CreateButton(frame, 55, 25, L["Preset"])
+	scroll.preset:SetPoint("RIGHT", scroll.clear, "LEFT", -5, 0)
+	scroll.preset.text:SetTextColor(1, .8, 0)
+	scroll.preset:SetScript("OnClick", function(self)
+		EasyMenu(menuList, B.EasyMenu, self, -100, 100, "MENU", 1)
+	end)
+
+	for spellID, duration in pairs(C.PartySpells) do
+		local name = GetSpellInfo(spellID)
+		if name then
+			local modDuration = NDuiADB["PartySpells"][spellID]
+			if not modDuration or modDuration > 0 then
+				createBar(scroll.child, spellID, duration)
+			end
+		end
+	end
+	for spellID, duration in pairs(NDuiADB["PartySpells"]) do
+		if duration > 0 then
+			createBar(scroll.child, spellID, duration)
+		end
 	end
 end
 
