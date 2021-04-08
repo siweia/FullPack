d7b3b5441faa9ea894eba84d5e42502e63cb6e55
siweia <siwei1990@outlook.com>
2021-04-09 01:30:35 +0800

    标记为6.9.9版本

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/LFGList.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/LFGList.lua
@@ -1,6 +1,56 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
+local function Highlight_OnEnter(self)
+	self.hl:Show()
+end
+
+local function Highlight_OnLeave(self)
+	self.hl:Hide()
+end
+
+local function HandleRoleAnchor(self, role)
+	self[role.."Count"]:SetWidth(24)
+	self[role.."Count"]:SetFontObject(Game13Font)
+	self[role.."Count"]:SetPoint("RIGHT", self[role.."Icon"], "LEFT", 1, 0)
+end
+
+local roleCache = {}
+local roleOrder = {
+	["TANK"] = 1,
+	["HEALER"] = 2,
+	["DAMAGER"] = 3,
+}
+local roleAtlas = {
+	[1] = "groupfinder-icon-role-large-tank",
+	[2] = "groupfinder-icon-role-large-heal",
+	[3] = "groupfinder-icon-role-large-dps",
+}
+
+local function sortRoleOrder(a, b)
+	if a and b then
+		return a[1] < b[1]
+	end
+end
+
+local function UpdateGroupRoles(self)
+	wipe(roleCache)
+
+	local count = 0
+	for i = 1, 5 do
+		local role, class = C_LFGList.GetSearchResultMemberInfo(self.resultID, i)
+		local roleIndex = role and roleOrder[role]
+		if roleIndex then
+			count = count + 1
+			if not roleCache[count] then roleCache[count] = {} end
+			roleCache[count][1] = roleIndex
+			roleCache[count][2] = class
+		end
+	end
+
+	sort(roleCache, sortRoleOrder)
+end
+
 tinsert(C.defaultThemes, function()
 	if not C.db["Skins"]["BlizzardSkins"] then return end
 
@@ -37,6 +87,14 @@ tinsert(C.defaultThemes, function()
 		end
 	end)
 
+	hooksecurefunc("LFGListSearchEntry_UpdateExpiration", function(self)
+		local expirationTime = self.ExpirationTime
+		if not expirationTime.fontStyled then
+			expirationTime:SetWidth(42)
+			expirationTime.fontStyled = true
+		end
+	end)
+
 	-- [[ Search panel ]]
 
 	local SearchPanel = LFGListFrame.SearchPanel
@@ -44,11 +102,7 @@ tinsert(C.defaultThemes, function()
 	B.Reskin(SearchPanel.RefreshButton)
 	B.Reskin(SearchPanel.BackButton)
 	B.Reskin(SearchPanel.SignUpButton)
-	if DB.isNewPatch then
-		B.Reskin(SearchPanel.ScrollFrame.ScrollChild.StartGroupButton)
-	else
-		B.Reskin(SearchPanel.ScrollFrame.StartGroupButton)
-	end
+	B.Reskin(SearchPanel.ScrollFrame.ScrollChild.StartGroupButton)
 	B.ReskinInput(SearchPanel.SearchBox)
 	B.ReskinScroll(SearchPanel.ScrollFrame.scrollBar)
 
@@ -56,14 +110,6 @@ tinsert(C.defaultThemes, function()
 	SearchPanel.RefreshButton.Icon:SetPoint("CENTER")
 	SearchPanel.ResultsInset:Hide()
 	B.StripTextures(SearchPanel.AutoCompleteFrame)
-
-	local function resultOnEnter(self)
-		self.hl:Show()
-	end
-
-	local function resultOnLeave(self)
-		self.hl:Hide()
-	end
 
 	local numResults = 1
 	hooksecurefunc("LFGListSearchPanel_UpdateAutoComplete", function(self)
@@ -92,8 +138,8 @@ tinsert(C.defaultThemes, function()
 			hl:Hide()
 			result.hl = hl
 
-			result:HookScript("OnEnter", resultOnEnter)
-			result:HookScript("OnLeave", resultOnLeave)
+			result:HookScript("OnEnter", Highlight_OnEnter)
+			result:HookScript("OnLeave", Highlight_OnLeave)
 
 			numResults = numResults + 1
 		end
@@ -104,14 +150,6 @@ tinsert(C.defaultThemes, function()
 	local ApplicationViewer = LFGListFrame.ApplicationViewer
 	ApplicationViewer.InfoBackground:Hide()
 	ApplicationViewer.Inset:Hide()
-
-	local function headerOnEnter(self)
-		self.hl:Show()
-	end
-
-	local function headerOnLeave(self)
-		self.hl:Hide()
-	end
 
 	for _, headerName in pairs({"NameColumnHeader", "RoleColumnHeader", "ItemLevelColumnHeader"}) do
 		local header = ApplicationViewer[headerName]
@@ -128,8 +166,8 @@ tinsert(C.defaultThemes, function()
 		hl:Hide()
 		header.hl = hl
 
-		header:HookScript("OnEnter", headerOnEnter)
-		header:HookScript("OnLeave", headerOnLeave)
+		header:HookScript("OnEnter", Highlight_OnEnter)
+		header:HookScript("OnLeave", Highlight_OnLeave)
 	end
 
 	ApplicationViewer.RoleColumnHeader:SetPoint("LEFT", ApplicationViewer.NameColumnHeader, "RIGHT", 1, 0)
@@ -179,7 +217,63 @@ tinsert(C.defaultThemes, function()
 			B.ReskinRole(self.HealerIcon, "HEALER")
 			B.ReskinRole(self.DamagerIcon, "DPS")
 
+			self.HealerIcon:SetPoint("RIGHT", self.DamagerIcon, "LEFT", -22, 0)
+			self.TankIcon:SetPoint("RIGHT", self.HealerIcon, "LEFT", -22, 0)
+
+			HandleRoleAnchor(self, "Tank")
+			HandleRoleAnchor(self, "Healer")
+			HandleRoleAnchor(self, "Damager")
+
 			self.styled = true
+		end
+	end)
+
+	hooksecurefunc("LFGListGroupDataDisplayEnumerate_Update", function(self, numPlayers)
+		UpdateGroupRoles(self:GetParent():GetParent())
+
+		for i = 1, 5 do
+			local icon = self.Icons[i]
+			if not icon.bg then
+				if i > 1 then
+					icon:ClearAllPoints()
+					icon:SetPoint("RIGHT", self.Icons[i-1], "LEFT", -4, 0)
+				end
+				icon:SetSize(22, 22)
+				icon.bg = B.CreateBDFrame(icon, .25)
+
+				icon.role = self:CreateTexture(nil, "OVERLAY")
+				icon.role:SetSize(18, 18)
+				icon.role:SetPoint("CENTER", icon, "BOTTOM")
+			end
+
+			if i > numPlayers then
+				icon.bg:Hide()
+				icon.role:Hide()
+			else
+				icon.bg:Show()
+				icon.role:Show()
+			end
+		end
+
+		local iconIndex = numPlayers
+		for i = 1, #roleCache do
+			local roleInfo = roleCache[i]
+			local icon = self.Icons[iconIndex]
+			if roleInfo then
+				icon:SetTexture("Interface\\GLUES\\CHARACTERCREATE\\UI-CHARACTERCREATE-CLASSES")
+				local tcoords = CLASS_ICON_TCOORDS[roleInfo[2]]
+				icon:SetTexCoord(tcoords[1] + .022, tcoords[2] - .025, tcoords[3] + .022, tcoords[4] - .025)
+				icon.role:SetAtlas(roleAtlas[roleInfo[1]])
+				icon.bg:Show()
+				iconIndex = iconIndex - 1
+			end
+		end
+
+		for i = 1, iconIndex do
+			local icon = self.Icons[i]
+			icon:SetTexCoord(0, 1, 0, 1)
+			icon.role:SetAtlas(nil)
+			icon.bg:Hide()
 		end
 	end)
 
