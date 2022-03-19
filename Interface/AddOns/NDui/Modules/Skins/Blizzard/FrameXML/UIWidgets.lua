d3a6e94be8697bc3d40e69b6dea3224bc06c0fb6
siweia <siwei1990@outlook.com>
2022-03-19 23:45:43 +0800

    NDui 6.20.9

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/UIWidgets.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/UIWidgets.lua
@@ -95,23 +95,36 @@ local function ReskinSpellDisplayWidget(spell)
 	spell.IconMask:Hide()
 end
 
+local function ReskinPowerBarWidget(self)
+	for _, widgetFrame in pairs(self.widgetFrames) do
+		if widgetFrame.widgetType == Type_StatusBar then
+			if not widgetFrame:IsForbidden() then
+				ReskinWidgetStatusBar(widgetFrame.Bar)
+			end
+		end
+	end
+end
+
+local function ReskinWidgetGroups(self)
+	for _, widgetFrame in pairs(self.widgetFrames) do
+		if not widgetFrame:IsForbidden() then
+			local widgetType = widgetFrame.widgetType
+			if widgetType == Type_DoubleStatusBar then
+				ReskinDoubleStatusBarWidget(widgetFrame)
+			elseif widgetType == Type_SpellDisplay then
+				ReskinSpellDisplayWidget(widgetFrame.Spell)
+			elseif widgetType == Type_StatusBar then
+				ReskinWidgetStatusBar(widgetFrame.Bar)
+			end
+		end
+	end
+end
+
 tinsert(C.defaultThemes, function()
 	if not C.db["Skins"]["BlizzardSkins"] then return end
 
-	hooksecurefunc(_G.UIWidgetTopCenterContainerFrame, "UpdateWidgetLayout", function(self)
-		for _, widgetFrame in pairs(self.widgetFrames) do
-			if not widgetFrame:IsForbidden() then
-				local widgetType = widgetFrame.widgetType
-				if widgetType == Type_DoubleStatusBar then
-					ReskinDoubleStatusBarWidget(widgetFrame)
-				elseif widgetType == Type_SpellDisplay then
-					ReskinSpellDisplayWidget(widgetFrame.Spell)
-				elseif widgetType == Type_StatusBar then
-					ReskinWidgetStatusBar(widgetFrame.Bar)
-				end
-			end
-		end
-	end)
+	hooksecurefunc(_G.UIWidgetTopCenterContainerFrame, "UpdateWidgetLayout", ReskinWidgetGroups)
+	ReskinWidgetGroups(_G.UIWidgetTopCenterContainerFrame)
 
 	hooksecurefunc(_G.UIWidgetBelowMinimapContainerFrame, "UpdateWidgetLayout", function(self)
 		for _, widgetFrame in pairs(self.widgetFrames) do
@@ -123,31 +136,10 @@ tinsert(C.defaultThemes, function()
 		end
 	end)
 
-	hooksecurefunc(_G.UIWidgetPowerBarContainerFrame, "UpdateWidgetLayout", function(self)
-		for _, widgetFrame in pairs(self.widgetFrames) do
-			if widgetFrame.widgetType == Type_StatusBar then
-				if not widgetFrame:IsForbidden() then
-					ReskinWidgetStatusBar(widgetFrame.Bar)
-				end
-			end
-		end
-	end)
+	hooksecurefunc(_G.UIWidgetPowerBarContainerFrame, "UpdateWidgetLayout", ReskinPowerBarWidget)
+	ReskinPowerBarWidget(_G.UIWidgetPowerBarContainerFrame)
 
-	for _, widgetFrame in pairs(_G.UIWidgetPowerBarContainerFrame.widgetFrames) do
-		if not widgetFrame:IsForbidden() then
-			ReskinWidgetStatusBar(widgetFrame.Bar)
-		end
-	end
-
-	hooksecurefunc(_G.TopScenarioWidgetContainerBlock.WidgetContainer, "UpdateWidgetLayout", function(self)
-		for _, widgetFrame in pairs(self.widgetFrames) do
-			if widgetFrame.widgetType == Type_StatusBar then
-				if not widgetFrame:IsForbidden() then
-					ReskinWidgetStatusBar(widgetFrame.Bar)
-				end
-			end
-		end
-	end)
+	hooksecurefunc(_G.TopScenarioWidgetContainerBlock.WidgetContainer, "UpdateWidgetLayout", ReskinPowerBarWidget)
 
 	hooksecurefunc(_G.BottomScenarioWidgetContainerBlock.WidgetContainer, "UpdateWidgetLayout", function(self)
 		for _, widgetFrame in pairs(self.widgetFrames) do
