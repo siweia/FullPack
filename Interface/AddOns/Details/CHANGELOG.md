74a98a104dd69f8d4d0c2dd0866c8abf539b6292
siweia <siwei1990@outlook.com>
2024-02-13 00:08:26 +0800

    Details 12294

--- Interface/AddOns/Details/CHANGELOG.md
+++ Interface/AddOns/Details/CHANGELOG.md
@@ -1,7 +1,11 @@
 # Details! Damage Meter
 
-## [Details.20240122.12237.155](https://github.com/Tercioo/Details-Damage-Meter/tree/Details.20240122.12237.155) (2024-01-22)
-[Full Changelog](https://github.com/Tercioo/Details-Damage-Meter/compare/Details.20240120.12236.155...Details.20240122.12237.155) 
+## [Details.20240208.12294.155](https://github.com/Tercioo/Details-Damage-Meter/tree/Details.20240208.12294.155) (2024-02-08)
+[Full Changelog](https://github.com/Tercioo/Details-Damage-Meter/compare/Details.20240207.12281.155...Details.20240208.12294.155) 
 
-- Tagging release for latest commit which fixed an issue with WotLK.  
-- Fix for an error when starting a boss encounter on Wrath  
+- Bug fixes for the Mythic+ End of Run panel  
+    - General fixes applied to the Mythic+ Panel.  
+    - The Mythic+ section in the options panel can now be translated.  
+    - More fixes for text color.  
+- Fixed an issue with text colors and the background bar color  
+- Renamed "Show Damage Charts" to "Show End of M+ Panel"  
--- Interface/AddOns/Details/core/parser.lua
+++ Interface/AddOns/Details/core/parser.lua
@@ -35,7 +35,7 @@
 	local shield_cache = Details.ShieldCache --details local
 	local parser = Details.parser --details local
 
-	local cc_spell_list = detailsFramework.CrowdControlSpells
+	local crowdControlSpells = LIB_OPEN_RAID_CROWDCONTROL
 	local spellContainerClass = Details.container_habilidades --details local
 
 	--localize the cooldown table from the framework
@@ -2981,7 +2981,7 @@
 			if (_in_combat) then
 				------------------------------------------------------------------------------------------------
 				--buff uptime
-				if (cc_spell_list [spellId]) then
+				if (crowdControlSpells[spellId]) then
 					parser:add_cc_done (token, time, sourceSerial, sourceName, sourceFlags, targetSerial, targetName, targetFlags, targetFlags2, spellId, spellName)
 				end
 
@@ -4374,7 +4374,7 @@ local SPELL_POWER_PAIN = SPELL_POWER_PAIN or (PowerEnum and PowerEnum.Pain) or 1
 
 	--serach key: ~cc
 	function parser:break_cc(token, time, sourceSerial, sourceName, sourceFlags, targetSerial, targetName, targetFlags, targetFlags2, spellId, spellName, spellType, extraSpellID, extraSpellName, extraSchool, auraType)
-		if (not cc_spell_list[spellId]) then
+		if (not crowdControlSpells[spellId]) then
 			return
 
 		elseif (bitBand(sourceFlags, AFFILIATION_GROUP) == 0) then
@@ -4833,6 +4833,13 @@ local SPELL_POWER_PAIN = SPELL_POWER_PAIN or (PowerEnum and PowerEnum.Pain) or 1
 		return Details.capture_real[captureType]
 	end
 
+	function Details:CaptureReset()
+		Details:CancelAllCaptureSchedules()
+		for _, thisType in ipairs(Details.capture_types) do
+			Details:CaptureSet(true, thisType, true)
+		end
+	end
+
 	function Details:CaptureSet(onOff, captureType, real, time)
 		if (onOff == nil) then
 			onOff = Details.capture_real[captureType]
@@ -5204,6 +5211,10 @@ local SPELL_POWER_PAIN = SPELL_POWER_PAIN or (PowerEnum and PowerEnum.Pain) or 1
 		Details.zone_name = zoneName
 
 		_in_resting_zone = IsResting()
+
+		if (_in_resting_zone) then
+			Details:CaptureReset()
+		end
 
 		parser:WipeSourceCache()
 		Details.listener:UnregisterEvent("UNIT_FLAGS")
@@ -5860,13 +5871,13 @@ local SPELL_POWER_PAIN = SPELL_POWER_PAIN or (PowerEnum and PowerEnum.Pain) or 1
 			Details222.MythicPlus.time = 0.1
 		end
 
-		if (level >= 28 or Details.user_is_patreon_supporter) then --debug
+		--if (level >= 28 or Details.user_is_patreon_supporter) then --debug
 			C_Timer.After(0, function()
 				if (ChallengeModeCompleteBanner) then
 					ChallengeModeCompleteBanner.timeToHold = 0.1
 				end
 			end)
-		end
+		--end
 
 		--send mythic dungeon end event
 		local zoneName, instanceType, difficultyID, difficultyName, maxPlayers, dynamicDifficulty, isDynamic, instanceMapID, instanceGroupSize = GetInstanceInfo()
