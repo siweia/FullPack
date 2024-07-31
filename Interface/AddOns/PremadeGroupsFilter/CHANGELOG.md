e03754a7af19374c06f1046734cb4fc9f515a3ca
siweia <siwei1990@outlook.com>
2024-07-31 22:25:57 +0800

    PGF 5.8.2

--- Interface/AddOns/PremadeGroupsFilter/CHANGELOG.md
+++ Interface/AddOns/PremadeGroupsFilter/CHANGELOG.md
@@ -1,6 +1,6 @@
 # Premade Groups Filter
 
-## [5.7.2](https://github.com/0xbs/premade-groups-filter/tree/5.7.2) (2024-07-01)
-[Full Changelog](https://github.com/0xbs/premade-groups-filter/compare/5.7.1...5.7.2) [Previous Releases](https://github.com/0xbs/premade-groups-filter/releases)
+## [5.8.2](https://github.com/0xbs/premade-groups-filter/tree/5.8.2) (2024-07-25)
+[Full Changelog](https://github.com/0xbs/premade-groups-filter/compare/5.8.1...5.8.2) [Previous Releases](https://github.com/0xbs/premade-groups-filter/releases)
 
-- Update italian translation from Grifo92  
+- More fixes for Lua error (see #267)  
--- Interface/AddOns/PremadeGroupsFilter/Init.lua
+++ Interface/AddOns/PremadeGroupsFilter/Init.lua
@@ -89,6 +89,7 @@ C.CATEGORY_ID = {
     WRATH_QUESTING     = 116,
     WRATH_BATTLEGROUND = 118,
     WRATH_CUSTOM       = 120,
+    DELVES             = 121,
 }
 
 C.DIFFICULTY_KEYWORD = {
@@ -179,7 +180,6 @@ C.SETTINGS_DEFAULT = {
     dialogMovable = true,
     classNamesInTooltip = true,
     coloredGroupTexts = true,
-    coloredApplications = true,
     ratingInfo = true,
     classCircle = false,
     classBar = false,
@@ -266,11 +266,19 @@ function PGF.MigrateSettingsV2()
     end
 end
 
+function PGF.MigrateSettingsV3()
+    if not PremadeGroupsFilterSettings.version or PremadeGroupsFilterSettings.version < 3 then
+        PremadeGroupsFilterSettings.coloredApplications = nil
+        PremadeGroupsFilterSettings.version = 3
+    end
+end
+
 function PGF.OnAddonLoaded(name)
     if name == PGFAddonName then
         -- update new settings with defaults
         PGF.Table_UpdateWithDefaults(PremadeGroupsFilterSettings, PGF.C.SETTINGS_DEFAULT)
         PGF.MigrateSettingsV2()
+        PGF.MigrateSettingsV3()
 
         -- initialize dialog state and migrate to latest version
         if PremadeGroupsFilterState == nil or PremadeGroupsFilterState.version == nil then
